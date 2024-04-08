8546563828014582ac83c2e424f218b281dc4dc1
Nabil <l4teoyt@gmail.com>
2024-04-08 16:24:26 +0200

    Ajout de l'exercice 12

---
+++ Partie1/exo12.php
@@ -0,0 +1,29 @@
+<h1>Exercice 12</h1>
+
+<p>A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée
+(tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue
+respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola »)
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$formateurs = [
+    "Mickael" => "FRA",
+    "Virgile" => "ESP",
+    "Marie-Claire" => "ENG"
+];
+
+//variante
+ksort($formateurs);
+
+foreach ($formateurs as $prenom => $langue) {
+    if($langue == "FRA") {
+        echo"Salut $prenom<br>";
+    } else if($langue == "ESP") {
+        echo "Hola $prenom<br>";
+    } else if($langue == "ENG") {
+        echo "Hello $prenom<br>";
+    }
+}
